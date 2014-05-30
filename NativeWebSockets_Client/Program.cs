802f79cd69a367a051a22c77b3107d3167f525bc
Srivathsan Madhavan <vachu@github.fake>
2014-05-30 18:01:35 +0530

    Separated the websocket operations into Extension Methods

--- NativeWebSockets_Client/Program.cs
+++ NativeWebSockets_Client/Program.cs
@@ -5,41 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.WebSockets;
 using System.Threading;
+using ClientWebSocketExtensions;
 
 namespace NativeWebSockets_Client
 {
     class Program
     {
-        const int BUFF_SIZE = 10 * 1024;
-        static ClientWebSocket m_ws = new ClientWebSocket();
-        static CancellationTokenSource m_tokSrc = new CancellationTokenSource();
-
         static void Main(string[] args)
         {
             try
             {
-                m_ws.Options.KeepAliveInterval = ClientWebSocket.DefaultKeepAliveInterval;
-                m_ws.Options.SetBuffer(BUFF_SIZE, BUFF_SIZE);
-
-                // open websocket connection
-                OpenWebSocket(new Uri("ws://localhost:7573")).Wait();
-                if (m_ws.State == WebSocketState.Open)
+                using (var ws = new ClientWebSocket())
                 {
-                    //Loop:
-                    //      Read console input
-                    //      If input is "close", "exit" or "quit", then break from this loop
-                    //
-                    //      Send the input to websocket server
-                    //      Receive response from websocket server
-                    //      Display response on console
-                    Send("List 100000").Wait();
+                    if (ws.Open(new Uri("ws://localhost:7573")))
+                    {
+                        ws.Send("List 1000000").Wait();
 
-                    var sb = new StringBuilder();
-                    Recv(sb).Wait();
-                    Console.WriteLine(sb.ToString());
+                        var sb = new StringBuilder();
+                        ws.Recv(sb).Wait();
+                        Console.WriteLine(sb.ToString());
+                    }
+                    ws.Close();
                 }
-                // close websocket conection
-                CloseWebSocket().Wait();
             }
             catch (AggregateException agg)
             {
@@ -47,13 +34,8 @@ namespace NativeWebSockets_Client
                 sb.Append(agg.Message);
                 foreach (var e in agg.InnerExceptions)
                 {
-                    sb.Append("\n");
+                    sb.Append("; ");
                     sb.Append(e.Message);
-                    if (e.InnerException != null)
-                    {
-                        sb.Append("\n");
-                        sb.Append(e.InnerException.Message);
-                    }
                 }
                 Console.Error.WriteLine("AGGREGATE EXCEPTION: {0}", sb.ToString());
             }
@@ -61,59 +43,6 @@ namespace NativeWebSockets_Client
             {
                 Console.Error.WriteLine("UNKNOWN EXCEPTION: {0}", e.Message);
             }
-            Console.Error.WriteLine("Client WebSocket State: {0}", m_ws.State.ToString());
-        }
-
-        static async Task OpenWebSocket(Uri wsUri)
-        {
-            await m_ws.ConnectAsync(wsUri, m_tokSrc.Token);
-        }
-
-        static async Task Send(string data)
-        {
-            await m_ws.SendAsync(
-                    new ArraySegment<byte>(Encoding.UTF8.GetBytes(data)),
-                    WebSocketMessageType.Text,
-                    true,
-                    m_tokSrc.Token
-                );
-        }
-
-        static async Task Recv(StringBuilder sb)
-        {
-            if (!IsWSUsable())
-                return;
-
-            var recvBuff = new ArraySegment<byte>(new byte[BUFF_SIZE]);
-            var res = await m_ws.ReceiveAsync(recvBuff, m_tokSrc.Token);
-            while (IsWSUsable() && res != null && res.MessageType != WebSocketMessageType.Close &&
-                    res.MessageType == WebSocketMessageType.Text && res.Count > 0)
-            {
-                var recvBytes = recvBuff.Array;
-                Array.Resize(ref recvBytes, res.Count);
-                var recvMsg = Encoding.UTF8.GetString(recvBytes);
-                if (res.EndOfMessage && String.CompareOrdinal(recvMsg, "==== EOT ====") == 0)
-                {
-                    break;
-                }
-                sb.Append(recvMsg);
-                Array.Resize(ref recvBytes, BUFF_SIZE);
-                res = await m_ws.ReceiveAsync(recvBuff, m_tokSrc.Token);
-            }
-        }
-
-        static async Task CloseWebSocket()
-        {
-            if (!IsWSUsable())
-                return;
-
-            await m_ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "", m_tokSrc.Token);
-            m_ws.Dispose();
-        }
-
-        static bool IsWSUsable()
-        {
-            return m_ws.State == WebSocketState.Open;
         }
     }
 }
---
+++ NativeWebSockets_Client/WSExtensions.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ClientWebSocketExtensions
+{
+    public static class CmdLineTextExtensions
+    {
+        const int WAIT_TIME = 15 * 1000; // 10 secs
+        const int BUFF_SIZE = 10 * 1000; // 10K buffer
+
+        public static bool Open(this ClientWebSocket ws, Uri wsUri)
+        {
+            using (var tokSrc = new CancellationTokenSource())
+            {
+                ws.Options.SetBuffer(BUFF_SIZE, BUFF_SIZE);
+                ws.Options.KeepAliveInterval = ClientWebSocket.DefaultKeepAliveInterval;
+                return (
+                        ws.ConnectAsync(wsUri, tokSrc.Token).Wait(WAIT_TIME) &&
+                        ws.State == WebSocketState.Open
+                    );
+            }
+        }
+
+        public static void Close(this ClientWebSocket ws)
+        {
+            if (!ws.IsOpen())
+                return;
+
+            using (var tokSrc = new CancellationTokenSource())
+            {
+                ws.CloseAsync(WebSocketCloseStatus.NormalClosure, "", tokSrc.Token).Wait(WAIT_TIME);
+            }
+        }
+
+        public static async Task Send(this ClientWebSocket ws, string data)
+        {
+            using (var tokSrc = new CancellationTokenSource())
+            {
+                await ws.SendAsync(
+                            new ArraySegment<byte>(Encoding.UTF8.GetBytes(data)),
+                            WebSocketMessageType.Text,
+                            true,
+                            tokSrc.Token
+                        );
+            }
+        }
+
+        public static async Task Recv(this ClientWebSocket ws, StringBuilder sb)
+        {
+            if (!ws.IsOpen())
+                return;
+
+            using (var tokSrc = new CancellationTokenSource())
+            {
+                var recvBuff = new ArraySegment<byte>(new byte[BUFF_SIZE]);
+                var res = await ws.ReceiveAsync(recvBuff, tokSrc.Token);
+                while (ws.IsOpen() && res != null &&
+                        res.MessageType != WebSocketMessageType.Close &&
+                        res.MessageType == WebSocketMessageType.Text && res.Count > 0)
+                {
+                    var recvBytes = recvBuff.Array;
+                    Array.Resize(ref recvBytes, res.Count);
+                    var recvMsg = Encoding.UTF8.GetString(recvBytes);
+                    if (res.EndOfMessage && String.CompareOrdinal(recvMsg, "==== EOT ====") == 0)
+                    {
+                        break;
+                    }
+                    sb.Append(recvMsg);
+                    Array.Resize(ref recvBytes, BUFF_SIZE);
+                    res = await ws.ReceiveAsync(recvBuff, tokSrc.Token);
+                }
+            }
+        }
+
+        public static bool IsOpen(this ClientWebSocket ws)
+        {
+            return ws.State == WebSocketState.Open;
+        }
+    }
+}
